ebe530c9d4b7c856d6e0c9d17e27d005cce4c9de
Cord Huchzermeyer <huchzi@email.de>
2018-05-19 19:19:49 +0200

    Modifications to make functions to create stimuli work.

--- R/WritePresetFile.R
+++ R/WritePresetFile.R
@@ -1,22 +1,24 @@
+#' @export
 createPresetFile <- function (dname, LEDgruppe, LEDcontast100, innenLum, aussenLum) {
   phase <- (LEDcontrast100 < 0) * 180
-  pfile <- cbind(t(abs(LEDcontrast100)), t(abs(LEDcontrast100 / 5)), 
+  pfile <- cbind(t(abs(LEDcontrast100)), t(abs(LEDcontrast100 / 5)),
                  rep(0, 4), t(abs(LEDcontrast100 / 5)))
   if (LEDgruppe == "außen") {
     pfile <- rbind(cbind(rep(0, 4), rep(0, 4), rep(0, 4), rep(0, 4)), pfile)
   } else {
     pfile <- rbind(pfile, cbind(rep(0, 4), rep(0, 4), rep(0, 4), rep(0, 4)))
   }
-  pfile <- data.frame(Y1 = rep("True", 8), Y2 = rep("Sinus", 8), 
-                      Y3 = rep(10, 8), Y4 = c(innenLum, aussenLum), 
+  pfile <- data.frame(Y1 = rep("True", 8), Y2 = rep("Sinus", 8),
+                      Y3 = rep(10, 8), Y4 = c(innenLum, aussenLum),
                       Y5 = c(rep(0, 4), phase), pfile)
-  
+
   write.table(pfile, paste(dname, ".pre", sep = ""), eol = ";\r\n", quote = F, dec = ",", sep = ";", col.names = F, row.names = F)
 }
 
+#' @export
 createPresetTable <- function (pfilesList, Kliste) {
-  
-  pTabelle <- data.frame(X = c("modRED", "modGREEN", "modBLUE", "modCYAN", 
+
+  pTabelle <- data.frame(X = c("modRED", "modGREEN", "modBLUE", "modCYAN",
                                "phaseRED", "phaseGREEN", "phaseBLUE", "phaseCYAN",
                                "K", "lty"))
   for (dname in pfilesList) {
@@ -36,4 +38,4 @@ createPresetTable <- function (pfilesList, Kliste) {
   write.table(pTabelle, "presets.csv", sep = ";", quote = F)
   return(pTabelle)
 }
-  
+
