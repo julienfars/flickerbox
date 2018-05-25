cf30fd9e4fa3f2913ee6294e5e567a3193b18e07
Cord Huchzermeyer <huchzi@email.de>
2018-05-25 06:51:26 +0200

    Changed ellipse plot from base to ggplot2.

--- R/plotEllipse.R
+++ R/plotEllipse.R
@@ -8,40 +8,53 @@ plotEllipse <- function(ellipse,
   X <- c(ellipse[, "X"],-ellipse[, "X"])
   Y <- c(ellipse[, "Y"],-ellipse[, "Y"])
 
-  plot(
-    X,
-    Y,
-    main = paste(freq, "Hz"),
-    xlab = "M-cone contrast",
-    ylab = "L-cone contrast",
-    asp = 1
-  )
-
-  abline(h = 0)
-  abline(v = 0)
+  output <-
+    ggplot2:qplot(
+      X,
+      Y,
+      main = paste(freq, "Hz"),
+      xlab = "M-cone contrast",
+      ylab = "L-cone contrast",
+      aps = 1
+    ) +
+    geom_point() +
+    geom_hline(aes(yintercept = 0)) +
+    geom_vline(aes(yintercept = 0))
 
   if (!is.null(ellipseModel)) {
     yr <- c()
     xr <- c()
 
     for (i in ellipseModel$k) {
-      yr <- c(yr,
-              ellipseFunction(i,
-                              ellipseModel$results$a1Estimate,
-                              ellipseModel$results$a2Estimate,
-                              ellipseModel$results$alphaEstimate,
-                              returnValue = "singleValues")[2])
-      xr <- c(xr,
-              ellipseFunction(i,
-                              ellipseModel$results$a1Estimate,
-                              ellipseModel$results$a2Estimate,
-                              ellipseModel$results$alphaEstimate,
-                              returnValue = "singleValues")[1])
+      yr <- c(
+        yr,
+        ellipseFunction(
+          i,
+          ellipseModel$results$a1Estimate,
+          ellipseModel$results$a2Estimate,
+          ellipseModel$results$alphaEstimate,
+          returnValue = "singleValues"
+        )[2]
+      )
+      xr <- c(
+        xr,
+        ellipseFunction(
+          i,
+          ellipseModel$results$a1Estimate,
+          ellipseModel$results$a2Estimate,
+          ellipseModel$results$alphaEstimate,
+          returnValue = "singleValues"
+        )[1]
+      )
     }
 
     xr2 <- c(xr, -xr)
     yr2 <- c(yr, -yr)
-    lines(xr2, yr2, col = "red")
+    output <-
+      output +
+      geom_lines(aes(x = xr2, y = yr2), color = "red")
   }
+
+  print(output)
 
 }
