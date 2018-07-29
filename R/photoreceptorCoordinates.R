5ed418cd31c007ad2dc4804ea3652abdcbdc9cce
Cord Huchzermeyer <huchzi@email.de>
2018-07-29 20:55:32 +0200

    More output by the getPhotoreceptorCoordinats function added.

--- R/photoreceptorCoordinates.R
+++ R/photoreceptorCoordinates.R
@@ -10,21 +10,32 @@ read.flimmerkiste <- function(filename, ConeFund)
     ) %>%
     dplyr::select(-7,-12)
 
+
+  dat <- strsplit(filename, split = "/")[[1]]
+  dat <- dat[length(dat)]
+  dat <- strsplit(substr(dat, 1, nchar(dat) - 4), split = "_")[[1]]
+  patid <- dat[1]
+  seye <- dat[length(dat) - 2]
+  date_of_exam <-  strptime(paste(dat[length(dat) - 1], dat[length(dat)]), "%Y-%m-%d %H%M%S")
+
   if(sum(rt[4:5, 3:6]) == 0) field <- "surround" else field <- "center"
 
   if(field == "surround")
   {
     lmean <- c(rt[1, 7:10])
     phase <- c(rt[3, 7:10])
+    freq <- c(rt[2, 7:10])
   }
   else {
     lmean <- c(rt[1, 3:6])
     phase <- c(rt[3, 3:6])
+    freq <- c(rt[2, 3:6])
   }
   lmean <- as.vector(unlist(lmean))
   phase <- as.vector(unlist(phase))
   phase[phase == 180] <- -1
   phase[phase == 0] <- 1
+  freq <- as.vector(unlist(freq))
 
   thresholdsLED <-
     rbind(D = rt[which(grepl("Down: Schwelle", rt[, 1])), 3:6] * phase,
@@ -32,7 +43,15 @@ read.flimmerkiste <- function(filename, ConeFund)
 
   LEDcontrast <- apply(thresholdsLED, 2, mean)
 
-  return(findPhotoreceptorContrasts(LEDcontrast, lmean, ConeFund))
+  return(
+    data.frame(
+      patid,
+      seye,
+      date_of_exam,
+      frequency = max(freq),
+      findPhotoreceptorContrasts(LEDcontrast, lmean, ConeFund)
+    )
+  )
 
 }
 
